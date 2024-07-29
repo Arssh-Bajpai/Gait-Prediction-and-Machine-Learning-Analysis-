7c0af0e1839eb9548a0ce420949b9babe5264625
Junxiang Lin <lin.junx@northeastern.edu>
2024-07-29 15:35:34 -0400

    added basic code for gradient boosting classifier

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from sklearn.datasets import load_iris
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score
 
-# Load the iris dataset
-iris = load_iris()
+wine = datasets.load_wine()
+X = wine['data']
+y = wine['target']
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
 
-# Initialize the Gradient Boosting Classifier
-gb_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=42)
+gbr = GradientBoostingClassifier()
+gbr.fit(X_train, y_train)
+print(cross_val_score(gbr, X_train, y_train, scoring='accuracy', cv=5))
+param_grid = {
+    'n_estimators': [10, 50, 100, 500],
+    'learning_rate': [0.0001, 0.001, 0.01, 0.1, 1.0],
+    'max_depth': [3, 5, 7, 9]
+}
 
-# Train the classifier
-gb_clf.fit(X_train, y_train)
+from sklearn.model_selection import GridSearchCV
+gbr2 = GridSearchCV(gbr, param_grid, cv = 3, n_jobs = 3)
+gbr2.fit(X_train, y_train)
+print(gbr2.best_params_)
+print(gbr2.best_score_)
 
-# Make predictions on the test set
-y_pred = gb_clf.predict(X_test)
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy of the Gradient Boosting Classifier: {accuracy:.3f}")
+
+
+
+
+
